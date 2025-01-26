3f59f84e53cc029cc82fd8782f5c9fac963dd1f8
Martin Vuille <jpmv27@yahoo.com>
2025-01-26 07:51:35 -0500

    Setting the clock on the BedJet can cause it to become unstable and hang, disconnect, or otherwise become unresponsive. This is especially true when automatically updating the BedJet clock on every time source sync. Automatic BedJet clock setting is disabled and a warning is issued any time the BedJet clock is set.

--- components/bedjet_jpmv27/bedjet_hub.cpp
+++ components/bedjet_jpmv27/bedjet_hub.cpp
@@ -459,8 +459,10 @@ void BedJetHub::send_local_time() {
 
 void BedJetHub::setup_time_() {
   if (this->time_id_ != nullptr) {
+#if 0 // Allow manual tine-setting but disable automatic time sync
     this->send_local_time();
     this->time_id_->add_on_time_sync_callback([this] { this->send_local_time(); });
+#endif
   } else {
     ESP_LOGI(TAG, "`time_id` is not configured: will not sync BedJet clock.");
   }
@@ -468,6 +470,8 @@ void BedJetHub::setup_time_() {
 #endif
 
 void BedJetHub::set_clock(uint8_t hour, uint8_t minute) {
+  ESP_LOGW(TAG, "Setting the clock on the BedJet can cause it to become unstable or unresponsive");
+
   if (!this->is_connected()) {
     ESP_LOGV(TAG, "[%s] Not connected, cannot send time.", this->get_name().c_str());
     return;
